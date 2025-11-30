21f33e6e08d462a2846fd1f4b3721677fa82ac97
Juno <sojae0306@gmail.com>
2025-11-30 23:34:21 +0900

    fontWeight Update

--- src/App.tsx
+++ src/App.tsx
@@ -92,7 +92,7 @@ const App: React.FC = () => {
     return (
         <div style={appContainerStyle}>
             {/* [Refactor] <Container> */}
-            <h1 style={{ fontWeight: 300, letterSpacing: "2px" }}>
+            <h1 style={{ fontWeight: 600, letterSpacing: "2px" }}>
                 {/* [Refactor] <Title> */}
                 PVE GOMOKU (업그레이드)
             </h1>
